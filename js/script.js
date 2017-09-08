7b1f94074a0bba163a5ed0d41410e79d2d72504d
kamil rajca <kamil.rajca@gmail.com>
2017-09-08 23:14:03 +0200

    dodanie sekcji carousel

--- js/script.js
+++ js/script.js
@@ -14,3 +14,20 @@ jQuery(function ($) {
         $("#navigations").toggleClass("scrolling", scrollTop > scrollingStartDistance);
     });
 });
+// carousel
+$(function(){
+  var carouselList = $("#carousel ul");
+
+  setInterval(changeSlide, 2000); //co 2 sek. wykona funkcję zmieniającą slajd. 
+  
+  function changeSlide(){
+    carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);    
+  }
+
+  function moveFirstSlide(){ 
+    var firstItem = carouselList.find("li:first");
+    var lastItem = carouselList.find("li:last");
+    lastItem.after(firstItem);
+    carouselList.css({marginLeft:0});
+  }
+});
