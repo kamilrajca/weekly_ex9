2da490305268f845017fc7f0b03497b2175aa5e2
kamil rajca <kamil.rajca@gmail.com>
2017-09-11 01:19:08 +0200

    dodanie zdjęć w galerii

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
-// 
+// płynne przewijanie menu
+ $('a').click(
+    function(event){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 800)});
+
+// hamburger
 $(document).ready(function(){
   $('.menu-toggler').click(function(){
     $('.menu').toggleClass('visible');
